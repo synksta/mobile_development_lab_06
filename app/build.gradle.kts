1debf7fb10d92688bf80882f9a455589af834db3
synksta <contriturbamentum@gmail.com>
2024-09-29 08:00:33 +0700

    CriminalIntent - Start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.mobile_development_lab_06"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.mobile_development_lab_06"
         minSdk = 24
